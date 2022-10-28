87a5417aa1c95675c58bff75693dc672f5aa6341
Bruno Cimbleris <bcimbleris@gmail.com>
2022-10-28 13:42:08 -0300

    cria migration

---
+++ src/database/migrations/20221028160726_user.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("user", function(table){
+    table.string("user_id").primary().notNullable();
+    table.string("name").notNullable();
+    table.string("email").notNullable();
+    
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    knex.schema.dropTable("user");
+  
+};
